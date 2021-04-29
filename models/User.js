2ec898effe045bc7a300bf2de5310a33f25c7b05
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-04-28 23:57:51 -0500

    Backend Working Code, Card 2 complete

--- models/User.js
+++ models/User.js
@@ -1,1 +1,20 @@
-import React from 'react';
+'use strict';
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const bookSchema = new Schema ({
+  name: String, 
+  description: String,
+  status: String,
+});
+
+const userSchema = new Schema ({
+ email: String,
+ books: [bookSchema],
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User; 
+
--- server.js
+++ server.js
@@ -1,15 +1,50 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const express  = require('express');
+const express = require('express');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const User = require('./models/user');
+
+const userInformation = new User({
+  email: 'Williamsjanthony15@yahoo.com',
+  books: [{
+    name: 'Kujo',
+    description: 'Attack of a killer rabid dog',
+    status: 'Recommend',
+  },
+  {
+    name: 'Old Man and the Sea',
+    description: 'Fishing Journey',
+    status: 'Not Recommended',
+  },
+  {
+    name: 'Federalist Papers',
+    description: 'Historical Documents',
+    status: 'Recommend',
+  }]
+});
+
+userInformation.save(function (err) {
+  if (err) console.err(err);
+  else console.log('Added User');
+});
 
 app.get('/', (req, res) => {
   res.send('Welcome to my bookBack');
 });
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`)); 
+app.get('/books', (req, res) => {
+  // res.send('User Model');
+  User.find((err, dataBaseResults) => {
+    res.send(dataBaseResults);
+  });
+});
+
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
